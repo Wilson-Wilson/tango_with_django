187f4f450bd07ae6b5a7ec690c021c68c1a12fdf
Jonathan Wilson <j.a.wilson96@hotmail.com>
2016-08-06 11:33:47 -0500

    got templates working

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.http import  HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey! <br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    # you could've also just put the value there instead of creating a variable
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse("Rango says here's the about page. <br/> <a href='/rango/'>Index</a>")
