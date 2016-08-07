b302cfc34ebbcef31ee1634bc87288ad1b784d32
Jonathan Wilson <j.a.wilson96@hotmail.com>
2016-08-06 21:00:51 -0500

    added about template

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here's the about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': "here's the about page."}
+    return render(request, 'rango/about.html', context_dict)
